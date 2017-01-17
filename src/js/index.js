31767980f233e9b072860fd90fe0d11e31de6ec2
zhangheng <424508303@qq.com>
2017-01-17 18:41:37 +0800

    1_17

--- src/js/index.js
+++ src/js/index.js
@@ -3,39 +3,37 @@ $(function(){
 	$.post("php/nav.php",function(response){
 		var res = JSON.parse(response);
 		res.forEach(function(item, idx, arr){
+			var $aLi = $('<li/>');
+			var $oA = $('<a/>').html(item.title).appendTo($aLi);
 			if (item.indexId == 1) {
-				var $aLi = $('<li/>').appendTo($('.nav1'));
-				var $oA = $('<a/>').addClass('LNavFirst').html(item.title).appendTo($aLi);
-
+				$aLi.appendTo($('.nav1'));
 				if (item.iconSrc) {
 					var $oImg = $('<img/>').attr("src",item.iconSrc).appendTo($oA);
 				}
 			};
 
 			if (item.indexId >= 2 && item.indexId <= 5) {
-				var $aLi = $('<li/>').appendTo($('.nav2'));
-				var $oA = $('<a/>').addClass('LNavSecond').html(item.title).appendTo($aLi);
+				$aLi.appendTo($('.nav2'));
+				if (item.iconSrc) {
+					var $oImg = $('<img/>').attr("src",item.iconSrc).appendTo($oA);
+				}
 			};
 
 			if (item.indexId >= 6 && item.indexId <= 16) {
-				var $aLi = $('<li/>').appendTo($('.nav3'));
-				var $oA = $('<a/>').addClass('LNavThree').html(item.title).appendTo($aLi);
-
+				$aLi.appendTo($('.nav3'));
 				if (item.iconSrc) {
 					var $oImg = $('<img/>').attr("src",item.iconSrc).appendTo($oA);
 				}
 			};
 
 			if (item.indexId > 16) {
-				var $aLi = $('<li/>').appendTo($('.nav4'));
-				var $oA = $('<a/>').addClass('LNavFour').html(item.title).appendTo($aLi);
+				$aLi.appendTo($('.nav4'));
 			}
-
 		})
 	});
 
 	//主页中间商品图片
-	$.post()
+	// $.post()
 
 
 })
